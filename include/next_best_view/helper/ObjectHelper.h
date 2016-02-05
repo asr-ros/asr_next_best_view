91e8f7284836e487ce7b115a0b2a738a07ee6f0f
Jocelyn Borella <jocelyn.borella@gmail.com>
2016-02-05 21:14:29 +0100

    adeed serivce call

--- include/next_best_view/helper/ObjectHelper.h
+++ include/next_best_view/helper/ObjectHelper.h
@@ -5,6 +5,7 @@
 #include <map>
 #include "object_database/ObjectMetaData.h"
 #include "world_model/GetRecognizerName.h"
+#include "world_model/GetIntermediateObjectWeight.h"
 
 namespace next_best_view {
     typedef object_database::ObjectMetaData::Response ObjectMetaDataResponse;
@@ -21,11 +22,18 @@ namespace next_best_view {
         public:
             ros::ServiceClient object_metadata_service_client;
             ros::ServiceClient recognizer_name_service_client;
+            ros::ServiceClient intermediate_object_weight_service_client;
+
             std::map<std::string, ObjectMetaDataResponsePtr> object_metadata_cache;
+            std::map<std::string, double> intermediate_object_cache;
 
             State() : mNodeHandle() {
-                object_metadata_service_client = mNodeHandle.serviceClient<object_database::ObjectMetaData>("/object_database/object_meta_data");
-                recognizer_name_service_client = mNodeHandle.serviceClient<world_model::GetRecognizerName>("/env/world_model/get_recognizer_name");
+                object_metadata_service_client = mNodeHandle.serviceClient
+                        <object_database::ObjectMetaData>("/object_database/object_meta_data");
+                recognizer_name_service_client = mNodeHandle.serviceClient
+                        <world_model::GetRecognizerName>("/env/world_model/get_recognizer_name");
+                intermediate_object_weight_service_client = mNodeHandle.serviceClient
+                        <world_model::GetRecognizerName>("/env/world_model/get_intermediate_object_weight");
             }
         };
 
@@ -66,6 +74,34 @@ namespace next_best_view {
                 statePtr->object_metadata_cache[objectTypeName] = responsePtr;
             }
             return responsePtr;
+        }
+
+        double getIntermediateObjectValue(std::string objectTypeName) {
+            boost::shared_ptr<State> statePtr = InstancePtr();
+            double weight;
+
+            if (!statePtr->intermediate_object_weight_service_client.exists()) {
+                return 0;
+            }
+
+            std::map<std::string, double>::iterator response_it = statePtr->
+                    intermediate_object_cache.find(objectTypeName);
+
+            if (response_it == statePtr->intermediate_object_cache.end()) {
+                world_model::GetIntermediateObjectWeight getIntermediateObjectWeight;
+                getIntermediateObjectWeight.request.object_type = objectTypeName;
+                statePtr->intermediate_object_weight_service_client.call(getIntermediateObjectWeight);
+
+                weight = getIntermediateObjectWeight.response.value;
+
+                statePtr->intermediate_object_cache[objectTypeName] = weight;
+            }
+            else
+            {
+                weight = response_it->second;
+            }
+
+            return weight;
         }
     };
 }
