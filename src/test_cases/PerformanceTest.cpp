1bd1b606328e2c9f319b2dcaf08813fac84229c8
Daniel Stroh <daniel.stroh@student.kit.edu>
2016-07-06 13:22:37 +0200

    fix performance test, add navigation_navigation launch files to test launch files

--- src/test_cases/PerformanceTest.cpp
+++ src/test_cases/PerformanceTest.cpp
@@ -109,6 +109,10 @@ public:
                     }
                     std::cout << "point cloud size " << apc.request.point_cloud.elements.size() << std::endl;
 
+
+                    //Resete den calculator vor jedem test
+                    mResetCalculatorClient.call(reca.request, reca.response);
+
                     ROS_INFO("Setze initiale Pose");
                     geometry_msgs::Pose initialPose;
                     initialPose.position.x = 1.64;
@@ -121,8 +125,7 @@ public:
                     initialPose.orientation.z = -0.265640665651;
                     this->setInitialPose(initialPose);
 
-                    //Resete den calculator vor jedem test
-                    mResetCalculatorClient.call(reca.request, reca.response);
+                    //ros::Duration(5).sleep();
                     // Setze PointCloud
                     if (!mSetPointCloudClient.call(apc.request, apc.response))
                     {
